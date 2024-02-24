acbf7dfa9fe0b13b3b7ec442920a6b45bd47304f
ADJADI <mradjadi.a@gmail.com>
2024-02-24 19:18:29 +0100

    Link troubles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./common/Nav/Navbar";
@@ -10,17 +10,31 @@ import About from "./components/Pages/About";
 import Main from "./components/home/Main";
 
 function App() {
+  const [isclicked, setIsClicked] = useState(false);
+
+  const toggleNavLink = () => {
+    setIsClicked(!isclicked);
+  };
+
   return (
     <div className="xl:px-32 cursor-pointer">
       <Navbar />
+      {isclicked ? <Main /> : ""}
       <Routes>
-        <Route path={"/"} />
-        <Route path={"/Main"} element={<Main />} />
-        <Route path={"/Collection"} element={<Collection />} />
-        <Route path={"/Men"} element={<Men />} />
-        <Route path={"/Women"} element={<Women />} />
-        <Route path={"/Contact"} element={<Contact />} />
-        <Route path={"/About"} element={<About />} />
+        <Route path={"/Main"} element={<Main />} onClick={toggleNavLink} />
+        <Route
+          path={"/Collection"}
+          element={<Collection />}
+          onClick={toggleNavLink}
+        />
+        <Route path={"/Men"} element={<Men />} onClick={toggleNavLink} />
+        <Route path={"/Women"} element={<Women />} onClick={toggleNavLink} />
+        <Route
+          path={"/Contact"}
+          element={<Contact />}
+          onClick={toggleNavLink}
+        />
+        <Route path={"/About"} element={<About />} onClick={toggleNavLink} />
       </Routes>
     </div>
   );
